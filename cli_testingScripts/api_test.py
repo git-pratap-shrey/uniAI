aac2a0cafe54962535e8c46862be563bea08729e
git-pratap-shrey <207022770+git-pratap-shrey@users.noreply.github.com>
2025-12-16 14:54:44 +0530

    fix api leak

--- cli_testingScripts/api_test.py
+++ cli_testingScripts/api_test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBbdu3ZKgWagy6R-2jWq_Ggym8REQyZeFY")
+genai.configure(api_key="")
 
 # List all models available to your API key
 for m in genai.list_models():
